4b57fe403d6198050c6dc00c4523c7ef2e9122db
Francisco Quintero <flquintero1@gmail.com>
2024-05-01 12:36:12 -0400

    chore: added logrocket env variable

--- plugins/Logrocket.tsx
+++ plugins/Logrocket.tsx
@@ -6,7 +6,7 @@ const Logrocket = () => {
     <>
       <Script src="https://cdn.logr-ingest.com/LogRocket.min.js" />
       <Script id="logrocket">
-        {`window.LogRocket && window.LogRocket.init('tg3fug/portfolio');`}
+        {`window.LogRocket && window.LogRocket.init(${process.env.LOGROCKET_ID});`}
       </Script>
     </>
   );
