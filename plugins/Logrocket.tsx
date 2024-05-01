04d32cb572e35546640329fd7f8d698b7a989395
Francisco Quintero <flquintero1@gmail.com>
2024-05-01 13:18:19 -0400

    chore: corrected env variables

--- plugins/Logrocket.tsx
+++ plugins/Logrocket.tsx
@@ -6,7 +6,7 @@ const Logrocket = () => {
     <>
       <Script src="https://cdn.logr-ingest.com/LogRocket.min.js" />
       <Script id="logrocket">
-        {`window.LogRocket && window.LogRocket.init(${process.env.LOGROCKET_ID});`}
+        {`window.LogRocket && window.LogRocket.init(${process.env.NEXT_PUBLIC_LOGROCKET_ID});`}
       </Script>
     </>
   );
