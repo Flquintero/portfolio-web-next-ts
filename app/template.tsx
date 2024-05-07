b07ac782c43389e374d8003d703c824a2b2a6579
Francisco Quintero <flquintero1@gmail.com>
2024-05-06 23:06:27 -0400

    test: removing logrocket

--- app/template.tsx
+++ app/template.tsx
@@ -2,7 +2,7 @@
 import { GoogleTagManager } from '@next/third-parties/google';
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  import('@/plugins/LogRocket');
+  // import('@/plugins/LogRocket');
   return (
     <main>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID as string} />
